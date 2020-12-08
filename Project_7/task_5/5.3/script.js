5e2692b14ac22677633aae185f0e11e4f28c6bd4
Воякин Георгий <voyaking@gmail.com>
2020-12-08 17:27:45 +0300

    Project_7 task_5.3 fix

--- Project_7/task_5/5.3/script.js
+++ Project_7/task_5/5.3/script.js
@@ -1,7 +1,9 @@
 function func() {
   if (document.getElementById('textField').value) {
-      document.getElementById('textField').style.border = "10px solid";
+      /*document.getElementById('textField').style.border = "10px solid";*/
+      document.getElementById('text').innerHTML = document.getElementById('textField').value;
   } else {
-    document.getElementById('textField').style.border = "0.8px solid";
+    /*document.getElementById('textField').style.border = "0.8px solid";*/
+    document.getElementById('text').innerHTML = "";
   }
 }
